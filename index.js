ab112f852d67c0d56f4022adc536aab1084e5d2a
Daniel <dannyblaze469@gmail.com>
2021-06-29 19:22:28 -0400

    commit tree 865f028159cebea37fc82d9e4af18b0aa5ac7c87

---
+++ index.js
@@ -0,0 +1,22 @@
+// require byebug
+
+// let divide = () => par1 / par2
+
+let divide = () => {
+    return 2000 / 100
+}
+// divide(2000, 100)
+
+// console.log(divide(2000,100))
+
+const square = (num) => {
+    return num**2
+}
+
+console.log(square(6))
+
+const add = (num1, num2) => {
+    return num1 + num2
+}
+
+console.log(add(7, 3))
